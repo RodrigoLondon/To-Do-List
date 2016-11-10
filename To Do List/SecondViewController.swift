958f4e8e1f684a032c3557751b1818a755004658
Lewis Jones <apple@apples-macbook.home>
2016-11-10 16:14:19 +0000

    added files for To Do List

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!)
+        
+        item.text = ""
+            
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,14 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        item.resignFirstResponder()
+        return true
+    }
 }
 
